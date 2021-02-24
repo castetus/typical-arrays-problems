b588621855289473c23340d6af016dbffe9649aa
castetus <kboikov@mail.ru>
2021-02-24 20:04:15 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
+function customSort(array){
+  return array.sort(function(a, b){
+    return a - b
+  })
+}
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0
+  }
+  return customSort(array)[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0
+  }
+  return customSort(array)[array.length - 1]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0
+  }
+  return array.reduce((acc, cur) => {
+    return acc + cur
+  }) / array.length
 }
